659f0a38b5794e396952fe369fe54407320b61a8
Lewaro <118760347+guesswho-jr@users.noreply.github.com>
2024-04-25 11:40:29 -0700

    Update editHandler.php
    Try it and notify me if it fails

--- dashboard/edit-profile/editHandler.php
+++ dashboard/edit-profile/editHandler.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 require_once "../../admin/scripts/classes.php";
@@ -7,52 +8,51 @@ session_start();
 $db = new DataBase();
 
 echo '<link rel="stylesheet" href="/static/bootstrap.min.css">';
-if (!
-(
-     isset($_POST["username"])&&
-    isset($_POST["full_name"])&&
-    isset($_POST["bio"])&&
-      isset($_POST["old_pwd"])&&
-      isset($_POST["new_pwd"])&&
-     isset($_POST["conf_pwd"])
-)){
+if (!(
+    isset($_POST["username"]) &&
+    isset($_POST["full_name"]) &&
+    isset($_POST["bio"]) &&
+    isset($_POST["old_pwd"]) &&
+    isset($_POST["new_pwd"]) &&
+    isset($_POST["conf_pwd"])
+)) {
     die("<div class='alert alert-danger'>Please resubmit your form</div>");
 }
-if (!empty($_POST["username"])){
-    if (!validateString($_POST["username"])){
+if (!empty($_POST["username"])) {
+    if (!validateString($_POST["username"])) {
         die("<div class='alert alert-danger'>Valdation error occured!</div>");
     }
     $db->executeSql("update users set username=? where id=?", [$_POST["username"], (int)$_SESSION["userid"]]);
     $_SESSION["username"] = $_POST["username"];
-}//username update
+} //username update
 
-if (!empty($_POST["old_pwd"] && !empty($_POST["new_pwd"]) && !empty($_POST["conf_pwd"]))) {
-    $user = $db->executeSql("Select password from users where id=?", [(int)$_SESSION["userid"]], true);
-    // die(print_r($user));
-    if (!password_verify($_POST["old_pwd"], $user[0]["password"])) {
-        die("<div class='alert alert-danger'>Incorrect password!</div>");
-    }
-    if ($_POST["new_pwd"] !== $_POST["conf_pwd"]) {
-        die("<div class='alert alert-danger'>Passwords don't match!</div>");
+if (!empty($_POST["old_pwd"])) {
+    if (!empty($_POST["new_pwd"]) && !empty($_POST["conf_pwd"])) {
+        $user = $db->executeSql("Select password from users where id=?", [(int)$_SESSION["userid"]], true);
+        // die(print_r($user));
+        if (!password_verify($_POST["old_pwd"], $user[0]["password"])) {
+            die("<div class='alert alert-danger'>Incorrect password!</div>");
         }
-        $new_hash = password_hash($_POST["conf_pwd"], PASSWORD_ARGON2ID); 
+        if ($_POST["new_pwd"] !== $_POST["conf_pwd"]) {
+            die("<div class='alert alert-danger'>Passwords don't match!</div>");
+        }
+        $new_hash = password_hash($_POST["conf_pwd"], PASSWORD_ARGON2ID);
         $db->executeSql("UPDATE users set password = ? WHERE id = ?", [$new_hash, (int)$_SESSION["userid"]]);
+    } else {
+        die("<div class='alert alert-danger'>If you want to submit the password you must specify all 3 password fields.</div>");
+    } // Password update
 }
-else {
-    die("<div class='alert alert-danger'>Please make sure that you submitted the password forms right!!</div>");
-} // Password update
 
-if (!empty($_POST["full_name"]))
-{
-    if (!validateStringWithSpaces($_POST["full_name"])){
+if (!empty($_POST["full_name"])) {
+    if (!validateStringWithSpaces($_POST["full_name"])) {
         die("<div class='alert alert-danger'>Validation error!</div>");
     }
-    $db->executeSql("UPDATE users SET full_name = ? WHERE id = ?", [ $_POST["full_name"], (int) $_SESSION["userid"]]);
+    $db->executeSql("UPDATE users SET full_name = ? WHERE id = ?", [$_POST["full_name"], (int) $_SESSION["userid"]]);
     $_SESSION["full_name"] = $_POST["full_name"];
 } // full_name update
 
 if (!empty($_POST["bio"])) {
-    if (!validateStringWithSpaces($_POST["bio"])){
+    if (!validateStringWithSpaces($_POST["bio"])) {
         die("<div class='alert alert-danger'>Validation error!</div>");
     }
     $db->executeSql("UPDATE users SET bio = ? WHERE id = ?", [$_POST["bio"], (int)$_SESSION["userid"]]);
@@ -61,7 +61,7 @@ if (!empty($_POST["bio"])) {
 header("Location: /dashboard");
 exit;
 // if (isset($_POST["submit"])) {
-    
+
 
 //     if (!empty($username) || !empty($full_name) || !empty($bio) || !empty($old_pwd) || !empty($new_pwd) || !empty($conf_pwd)) {
 //         $result = $db->executeSql("SELECT * FROM users WHERE id = ?", [(int)$_SESSION["userid"]], true);
@@ -84,4 +84,4 @@ exit;
 //     }
 // }
 
-// ?>
+// 
